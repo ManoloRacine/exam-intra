9b2f93ec944b62fe2c853105fb8ec500086f3833
ManoloRacine <manolo191202@gmail.com>
2022-10-24 11:27:25 -0400

    first commit

---
+++ src/test/java/com/example/exam_intra/ExamIntraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.exam_intra;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class ExamIntraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
